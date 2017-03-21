cb472287580ec536839973cfb5448940e9054ad2
leknaat <wishfire@gmail.com>
2017-03-21 10:26:16 +0000

    Oops, didn't lint! fixed.

--- src/redux/actions/todos.js
+++ src/redux/actions/todos.js
@@ -1,19 +1,21 @@
+import _ from 'lodash'
+
 export const addTodo = (todo) => {
   return (dispatch) =>
     dispatch({ type: 'todos/ADD_TODO', payload: todo })
-  }
+}
 
 export const updateTodoText = (todoText) => {
   return (dispatch) =>
     dispatch({ type: 'todos/UPDATE_TODO_TEXT', payload: todoText })
-  }
+}
 
 export const completeTodo = (todoId) => {
   return (dispatch) =>
     _.delay(() => dispatch({ type: 'todos/COMPLETE_TODO', payload: todoId }), 2000)
-  }
+}
 
 export const deleteTodo = (todoId) => {
   return (dispatch) =>
     dispatch({ type: 'todos/DELETE_TODO', payload: todoId })
-  }
+}
